90df6b332bd7d29686becb3eca8baf520aa97621
Taylor Perry <31444185+Pyrocamper@users.noreply.github.com>
2020-11-16 20:28:19 -0500

    Create Arduino_RFID.ino
    Code that is to be ran in Arduino IDE complier.

---
+++ Arduino_RFID.ino
@@ -0,0 +1,172 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(3, 2, A0, A1, A2, A3);
+ 
+#include <MFRC522.h> // for the RFID
+#include <SPI.h> // for the RFID and SD card module
+#include <SD.h> // for the SD card
+#include <RTClib.h> // for the RTC
+ 
+// define pins for RFID
+#define CS_RFID 10
+#define RST_RFID 9
+// define select pin for SD card module
+#define CS_SD 4 
+ 
+// Create a file to store the data
+File myFile;
+ 
+// Instance of the class for RFID
+MFRC522 rfid(CS_RFID, RST_RFID); 
+ 
+// Variable to hold the tag's UID
+String uidString;
+ 
+// Instance of the class for RTC using library DST RTC. 
+RTC_DS1307 rtc;
+ 
+// Define check in time
+const int checkInHour = 9;
+const int checkInMinute = 5;
+ 
+//Variable to hold user check in
+int userCheckInHour;
+int userCheckInMinute;
+ 
+// Pins for LEDs and buzzer
+//const int redLED = 6;
+const int buzzer = 5;
+ 
+void setup() {
+  
+  // Set LEDs and buzzer as outputs
+//  pinMode(redLED, OUTPUT);  
+//  pinMode(greenLED, OUTPUT);
+  pinMode(buzzer, OUTPUT);
+  
+  // Init Serial port
+  Serial.begin(9600);
+  lcd.begin(16,2);
+  while(!Serial); // for Leonardo/Micro/Zero
+  
+  // Init SPI bus
+  SPI.begin(); 
+  // Init MFRC522 
+  rfid.PCD_Init(); 
+ 
+  // Setup for the SD card
+  Serial.print("Initializing SD card...");
+  lcd.print("Initializing ");
+  lcd.setCursor(0, 1);
+  lcd.print("SD card...");
+  lcd.clear();
+  if(!SD.begin(CS_SD)) {
+    Serial.println("initialization failed!");
+    lcd.print("Initializing ");
+    lcd.setCursor(0, 1);
+    lcd.print("failed!");
+    return;
+  }
+  Serial.println("initialization done.");
+  lcd.print("Initialization ");
+  //setcursor seems to reset the screen or clearscreen
+  lcd.setCursor(0, 1);
+  lcd.print("Done...");
+ 
+  // Setup for the RTC  
+  if(!rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    lcd.clear();
+    lcd.print("Couldn't find RTC");
+    while(1);
+  }
+  else {
+    // following line sets the RTC to the date & time this sketch was compiled
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  }
+  if(!rtc.isrunning()) {
+    Serial.println("RTC is NOT running!");
+    lcd.clear();
+    lcd.print("RTC Not Running!");
+  }
+}
+ 
+void loop() {
+  //look for new cards
+  if(rfid.PICC_IsNewCardPresent()) {
+    readRFID();
+    logCard();
+    
+  }
+  delay(10);
+}
+ 
+void readRFID() {
+  rfid.PICC_ReadCardSerial();
+  lcd.clear();
+  Serial.print("Tag UID: ");
+  lcd.print("Tag UID: ");
+  uidString = String(rfid.uid.uidByte[0]) + " " + String(rfid.uid.uidByte[1]) + " " + 
+    String(rfid.uid.uidByte[2]) + " " + String(rfid.uid.uidByte[3]);
+  Serial.println(uidString);
+  lcd.setCursor(0, 1);
+  lcd.print(uidString);
+  
+ 
+  // Sound the buzzer when a card is read
+  tone(buzzer, 2000); 
+  delay(200);        
+  noTone(buzzer);
+  
+  delay(200);
+}
+ 
+void logCard() {
+  // Enables SD card chip select pin
+  digitalWrite(CS_SD,LOW);
+  
+  // Open file called tag list
+  myFile=SD.open("RFIDtag.txt", FILE_WRITE);
+ 
+  // If the file opened ok, write to it
+  if (myFile) {
+    Serial.println("File opened ok");
+    lcd.clear();
+    lcd.print("File opened ok");
+   
+    myFile.println(uidString);
+    
+    
+    // Save time on SD card
+    
+  DateTime now = rtc.now();
+
+    
+    // Print time on Serial monitor
+    Serial.print(now.year(), DEC);
+    Serial.print('/');
+    Serial.print(now.month(), DEC);
+    Serial.print('/');
+    Serial.print(now.day(), DEC);
+    Serial.print(' ');
+    Serial.print(now.hour(), DEC);
+    Serial.print(':');
+    Serial.println(now.minute(), DEC);
+    Serial.println("sucessfully written on SD card")
+
+    
+    myFile.close();
+ 
+    // Save check in time;
+    userCheckInHour = now.hour();
+    userCheckInMinute = now.minute();
+  }
+  else {
+    
+    Serial.println("error opening data.txt");  
+    lcd.clear();
+    lcd.print("error opening data.txt");
+  }
+  // Disables SD card chip select pin  
+  digitalWrite(CS_SD,HIGH);
+}
+ 
